7317e541e8c66dec03843186dcb02485ba424c90
William Díaz <wdiazux@gmail.com>
2013-06-07 17:29:31 -0600

    style changes

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -91,20 +91,20 @@
   <div class="container">
 
     <div class="content-inner">
-    <?php print render($page['highlighted']); ?>
-    <a id="main-content"></a>
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?>
-    <h1 class="page--title title" id="page-title"><?php print $title; ?></h1>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
-    <?php print $messages; ?>
-    <?php print render($tabs); ?>
-    <?php print render($page['help']); ?>
-    <?php if ($action_links): ?>
-    <ul class="action-links"><?php print render($action_links); ?></ul>
-    <?php endif; ?>
-    <?php print render($page['content']); ?>
+      <?php print render($page['highlighted']); ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+      <h1 id="page-title"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+      <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
     
     </div><!-- /content-inner -->
     

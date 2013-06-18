9f5ea9077a73c2f045bade679353634f84496666
William Díaz <wdiazux@gmail.com>
2013-06-18 15:42:57 -0600

    alerts and headings modifications

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -95,7 +95,7 @@
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
-      <h1 id="page-title"><?php print $title; ?></h1>
+      <h3 id="page-title"><?php print $title; ?></h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>

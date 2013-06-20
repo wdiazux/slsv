adb19d34d81c5eb8bff273ecb2977ff8825dfe9d
William Díaz <wdiazux@gmail.com>
2013-06-20 03:18:27 -0600

    Cleanup

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -48,7 +48,7 @@
         
         <?php endif; ?>
 
-        <div id="menu-icon">
+        <div id="btn-responsive">
           <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
